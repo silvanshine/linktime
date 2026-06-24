5e6f27af83e3f57bd45a90e0dcaabf17e178cb7b
Matt Mastracci <matthew@mastracci.com>
2026-06-24 10:01:51 -0600

    Fix mac/linux expansion

--- link-section/tests/expand-darwin/link_section.expanded.rs
+++ link-section/tests/expand-darwin/link_section.expanded.rs
@@ -19,7 +19,7 @@ impl FOO {
                     {
                         #[allow(missing_unsafe_on_extern)]
                         extern "C" {
-                            #[link_name = "\u{1}section$start$__DATA$FOO74oV7cnUspp"]
+                            #[link_name = "\u{1}section$start$__DATA$FOO06Y_1qTvDbT"]
                             static __SYMBOL: u8;
                         }
                         unsafe { &raw const __SYMBOL as *const () }
@@ -27,14 +27,14 @@ impl FOO {
                     {
                         #[allow(missing_unsafe_on_extern)]
                         extern "C" {
-                            #[link_name = "\u{1}section$end$__DATA$FOO74oV7cnUspp"]
+                            #[link_name = "\u{1}section$end$__DATA$FOO06Y_1qTvDbT"]
                             static __SYMBOL: u8;
                         }
                         unsafe { &raw const __SYMBOL as *const () }
                     },
                 )
             };
-            let name = "__DATA,FOO74oV7cnUspp";
+            let name = "__DATA,FOO06Y_1qTvDbT";
             ::link_section::__support::validate_section_name(name);
             unsafe { <TypedSection<fn()>>::new(name, section) }
         };
@@ -76,7 +76,7 @@ fn foo() {
         type __InSecStoredTy = <FOO as ::link_section::__support::SectionItemType>::Item;
         const __LINK_SECTION_CONST_ITEM_VALUE: __InSecStoredTy = foo;
         #[used]
-        #[link_section = "__DATA,FOO74oV7cnUspp,regular,no_dead_strip"]
+        #[link_section = "__DATA,FOO06Y_1qTvDbT,regular,no_dead_strip"]
         static __LINK_SECTION_CONST_ITEM: __InSecStoredTy = __LINK_SECTION_CONST_ITEM_VALUE;
         __LINK_SECTION_CONST_ITEM_VALUE
     };
